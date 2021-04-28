0a016b9288d512f0aae2b7f11ef84670d4be08ed
hoangqwe159 <doviethoang159@gmail.com>
2021-04-28 15:02:27 +1000

    Initialize

---
+++ Prac_1/hilbert_points.m
@@ -0,0 +1,13 @@
+function p = hilbert_points(n)
+%hilbert_points Calculate total number of points in n-th order 
+% Hilbert Curve
+
+[x, y] = hilbert(n);
+p = 2; %start and end points
+for n = 2:length(x)-1
+    if ~((x(n) == x(n-1) && x(n) == x(n+1))  || (y(n) == y(n-1) && y(n)== y(n+1))) 
+        p = p + 1;
+    end
+end
+end
+
